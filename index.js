ae26c9c32a981e3e26bb2f02faeaf1e2740a2ad0
Victoria <vmcdaniel459@gmail.com>
2024-06-20 18:15:47 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Vic',
+    streetAddress: '1009 Cumbermeade Rd.',
+};
+employee;
+function updateEmployeeWithKeyAndValue(employee, key, value) { 
+    const updatedEmployee = {...employee}
+    updatedEmployee.name = 'Sam'
+    updatedEmployee.streetAddress = '11 Broadway'
+    return updatedEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = 'Sam'
+    employee.streetAddress = '12 Broadway';
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeDeleted = {...employee}
+    delete employeeDeleted.name;
+    return employeeDeleted;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name
+    return employee;
+}
+
+
+
